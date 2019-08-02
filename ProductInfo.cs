0044daff54b0d6bbccb13063d83c9ade9b4d8e5a
Andrew Bibichev <bibigone@gmail.com>
2019-08-02 15:05:50 +0300

    Version 0.5.0

---
+++ ProductInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("K4A.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
