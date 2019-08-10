a936fbd03aab25c4d03e3db859d0b1982ed99a9e
Vasili Maslov <bassill@yandex.ru>
2019-08-11 01:37:43 +0300

    Unity sample: quit app on Esc key.

---
+++ K4AdotNet.Samples.Unity/Assets/Scripts/ApplicationController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace K4AdotNet.Samples.Unity
+{
+    public class ApplicationController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey("escape"))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
