c2398435488e34989419a6cc3f4cb529543c9984
Andrew Bibichev <bibigone@gmail.com>
2019-08-28 10:28:08 +0300

    Version 1.0.0

--- ProductInfo.cs
+++ ProductInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
