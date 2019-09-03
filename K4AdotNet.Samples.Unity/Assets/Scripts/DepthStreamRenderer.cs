4d77a7cd97b9465f8782eac4450f8deba015bb19
Vasili Maslov <bassill@yandex.ru>
2019-09-03 17:17:25 +0300

    Unity sample: added depth stream renderer.

---
+++ K4AdotNet.Samples.Unity/Assets/Scripts/DepthStreamRenderer.cs
@@ -0,0 +1,58 @@
+﻿using K4AdotNet.Sensor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace K4AdotNet.Samples.Unity.Assets.Scripts
+{
+    public class DepthStreamRenderer : MonoBehaviour
+    {
+        public float Alpha;
+        public Material DepthToColorMaterial;
+
+        private Texture2D _depthTexture;
+        private RenderTexture _renderTexture;
+
+        private void Start()
+        {
+            GetComponent<CanvasRenderer>().SetAlpha(Alpha);
+
+            var captureManager = FindObjectOfType<CaptureManager>();
+            if (captureManager.IsInitialized)
+            {
+                var frameWidth = captureManager.Configuration.DepthMode.WidthPixels();
+                var frameHeight = captureManager.Configuration.DepthMode.HeightPixels();
+
+                _depthTexture = new Texture2D(frameWidth, frameHeight, TextureFormat.R16, false);
+
+                _renderTexture = new RenderTexture(frameWidth, frameHeight, 0);
+                _renderTexture.Create();
+
+                GetComponent<RawImage>().texture = _renderTexture;
+
+                captureManager.CaptureReady += CaptureManager_CaptureReady;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            var captureManager = FindObjectOfType<CaptureManager>();
+            if (captureManager != null) captureManager.CaptureReady -= CaptureManager_CaptureReady;
+            Destroy(_depthTexture);
+            _renderTexture?.Release();
+        }
+
+        private void CaptureManager_CaptureReady(object sender, CaptureEventArgs e)
+        {
+            using (var depthImage = e.Capture.DepthImage)
+            {
+                if (depthImage != null)
+                {
+                    _depthTexture.LoadRawTextureData(depthImage.Buffer, depthImage.SizeBytes);
+                    _depthTexture.Apply();
+
+                    Graphics.Blit(_depthTexture, _renderTexture, DepthToColorMaterial);
+                }
+            }
+        }
+    }
+}
---
+++ K4AdotNet.Samples.Unity/Assets/Shaders/DepthToColor.shader
@@ -0,0 +1,57 @@
+﻿Shader "Hidden/DepthToColor"
+{
+    Properties
+    {
+		[NoScaleOffset] _MainTex ("Texture", 2D) = "black" {}
+		_MinDepth ("Min Depth (mm)", Int) = 0
+		_MaxDepth ("Max Depth (mm)", Int) = 5000
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+			int _MinDepth;
+			int _MaxDepth;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed depth = tex2D(_MainTex, i.uv).r;
+				depth = clamp(depth * 65535, _MinDepth, _MaxDepth);
+				fixed c = (_MaxDepth - depth) / (_MaxDepth - _MinDepth);
+				fixed a = depth == 0 ? 0 : 1;
+				return fixed4(c, c, c, a);
+            }
+            ENDCG
+        }
+    }
+}
